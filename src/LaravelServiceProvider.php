242babcc7cf625145e52f572b2aac8be1c6542a1
Jamie Holcroft <jholcroft@gmail.com>
2015-06-26 23:02:43 +0100

    Added support for repository injetion

--- src/LaravelServiceProvider.php
+++ src/LaravelServiceProvider.php
@@ -22,9 +22,38 @@ class LaravelServiceProvider extends ServiceProvider
      * @return void
      */
 
+
+    public function bindInterface()
+    {
+        $this->app->bind('Jampot5000\Calibre\Config\CalibreConfig', 'Jampot5000\Calibre\Config\LaravelCalibreConfig');
+        $this->app->bind('Jampot5000\Calibre\Repositories\Interfaces\AuthorRepository',
+                         'Jampot5000\Calibre\Repositories\Eloquent\AuthorRepository');
+        $this->app->bind('Jampot5000\Calibre\Repositories\Interfaces\BookRepository',
+                         'Jampot5000\Calibre\Repositories\Eloquent\BookRepository');
+    }
+
+    public function bindRepositories()
+    {
+        $this->app->bind('Jampot5000\Calibre\Repositories\Eloquent\AuthorRepository', function ($app) {
+            return $app['CalibreInterface']->getAuthorRepository();
+        });
+        $this->app->bind('Jampot5000\Calibre\Repositories\Eloquent\BookRepository', function ($app) {
+            return $app['CalibreInterface']->getBookRepository();
+        });
+    }
+
     public function register()
     {
+
+        $this->bindInterface();
+
+        $this->app->bind('Jampot5000\Calibre\Config\LaravelCalibreConfig', function () {
+           return new LaravelCalibreConfig($this->app->config['calibre']);
+        });
+
         $this->app->instance('CalibreInterface',new CalibreInterface(new LaravelCalibreConfig($this->app->config['calibre'])));
+
+        $this->bindRepositories();
     }
 
     private function setupConfig()
